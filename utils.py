0c70e6b56b2f6ad50e02df2b0b8b5345ca012cb7
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-11-28 20:56:08 +0200

    Random intial Data

--- utils.py
+++ utils.py
@@ -10,3 +10,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 #     most_similar_indices = np.argpartition(-similarities, k, axis=1)[:, :k]
 #     k_most_similar_vectors = data[most_similar_indices]
 #     return most_similar_indices,k_most_similar_vectors
+
+
+
+def generate_random(k=100):
+    # Sample data: 10 vectors with 70 features each
+    data = np.random.uniform(-1, 1, size=(k, 70))
+
+    # Write data to a text file
+    file_path = "random_data.txt"
+    np.savetxt(file_path, data)
+    read_data = np.loadtxt(file_path)
+    print(read_data)
+
+generate_random(100)
