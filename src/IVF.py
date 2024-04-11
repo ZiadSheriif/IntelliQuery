f128f6fe8c7bf0905d5dcb2fc15df3ed6e64fe87
ZiadSheriif <zsherif308@gmail.com>
2024-04-11 13:54:23 +0200

    chore: Remove useless functions

--- src/IVF.py
+++ src/IVF.py
@@ -1,9 +1,10 @@
+# import files
 from utils import *
-# from sklearn.cluster import MiniBatchKMeans
 from sklearn.cluster import KMeans
 
+# improt libraries
 import heapq
-import time
+import numpy as np
 
 
 def IVF_index(file_path,K_means_metric,K_means_n_clusters,k_means_batch_size,k_means_max_iter,k_means_n_init,chunk_size,index_folder_path):
@@ -25,14 +26,12 @@ def IVF_index(file_path,K_means_metric,K_means_n_clusters,k_means_batch_size,k_m
     # ############################################################### Step(1):Clustering Data from file ###############################################################
     # ############################################################### ################################# ###############################################################
     kmeans = KMeans(n_clusters=K_means_n_clusters, max_iter=k_means_max_iter,n_init=k_means_n_init,random_state=42)
-    # kmeans=AgglomerativeClustering(n_clusters=K_means_n_clusters,linkage='average',metric=K_means_metric)
-    # kmeans = MiniBatchKMeans(n_clusters=K_means_n_clusters, batch_size=k_means_batch_size, max_iter=k_means_max_iter,n_init=k_means_n_init,random_state=42)
+    
 
     # Use the first Chunck to only get teh centroids
     data_chunk=read_binary_file_chunk(file_path=file_path,record_format=f"I{70}f",start_index=0,chunk_size=1000000) #[{"id":,"embed":[]}]
     # TODO Remove this loop
     chunk_vectors=np.array([entry['embed'] for entry in data_chunk])
-    # kmeans.partial_fit(chunk_vectors)
     kmeans.fit(chunk_vectors)
 
 
@@ -59,16 +58,11 @@ def IVF_index(file_path,K_means_metric,K_means_n_clusters,k_means_batch_size,k_m
     # ##################################################################
     # #TEST# Centroids are Written Correct #############################
     # ##################################################################
-    # K_means_centroids_check=read_binary_file(index_folder_path+'/centroids.bin',f"{70}f")
-    # print("Note: Some Approximations happen due to float ")
-    # print(K_means_centroids_check[2])
-    # print(K_means_centroids[2])
 
 
 
     # Step(2) Getting vectors of each regions
     for i in range(no_chunks):
-        # print("IVF_indexing Chunk",i,".......")
         data_chunk=read_binary_file_chunk(file_path=file_path,record_format=f"I{70}f",start_index=i*chunk_size,chunk_size=chunk_size,dictionary_format=True) #[{109: np.array([70 dim])}]
 
         # Get Cluster for each one
@@ -83,75 +77,40 @@ def IVF_index(file_path,K_means_metric,K_means_n_clusters,k_means_batch_size,k_m
         for label in set(labels):
             region_ids=ids[labels==label]  # get ids belonging to such region
             region_vectors=vectors[labels==label]  # get vectors belonging to such region
-            # print(region_vectors.shape)
             # Open file of this Region(cluster) Just Once for every Region :D
             with open(index_folder_path+f'/cluster{label}.bin', "ab") as fout:
                 for i in range(len(region_ids)):
                     #TODO Check whether store id of the vector @Basma Elhoseny
                     data = struct.pack(f"I{70}f", region_ids[i],*region_vectors[i,:])
                     fout.write(data)
-
     
             
-                
-    ##################################################################
-    #TEST# no. of elements in each cluster ###########################
-    ##################################################################
-    # count_region=[]
-    # for region in range(K_means_n_clusters):
-    #     with open(index_folder_path+f'/cluster{region}.bin', "ab") as fout:
-    #             file_size = os.path.getsize(index_folder_path+f'/cluster{region}.bin')
-    #             record_size=struct.calcsize(f"I{70}f")
-    #             n_records=file_size/record_size
-
-    #             count_region.append(n_records)
-
-    # print(count_region) 
-    # print(np.sum(np.array(count_region)))
+            
     return
 
-import numpy as np
-import heapq
+
 
 def semantic_query_ivf(data_file_path, index_folder_path, query, top_k, n_regions):
-    print("semantic_query_ivf ()")
-
     query = np.squeeze(np.array(query))
 
-    # measure the time
-    start = time.time()
-    # Read Centroids
-    K_means_centroids = read_binary_file(index_folder_path + '/centroids.bin', f"70f") 
-    print("# of Centrodis: ",K_means_centroids.shape[0])
-    print("# of regions: ",n_regions)
 
+    # Read Centroids
+    K_means_centroids = read_binary_file(index_folder_path + '/centroids.bin', f"70f")     
 
 
     assert K_means_centroids.shape[0] > n_regions, "n_regions must be less than the number of regions"
-    # measure the time
-    # end = time.time()
-    # print("Time Stamp(1) ",end - start)
 
-    # measure the time
-    # start = time.time()
+
     # Calculate distances to centroids
     distances = np.linalg.norm(K_means_centroids - query, axis=1)
     # Get indices of the nearest centroids
     nearest_regions = np.argsort(distances)[:n_regions]
-    # measure the time
-    # end = time.time()
-    # print("Time Stamp(2) ",end - start)
+    
 
-    # measure the time
-    # start = time.time()
     # Use a heap to keep track of the top k scores
     top_scores_heap = []
     for region in nearest_regions:
-        start_loop = time.time()
-        # region_ids = read_binary_file(file_path=index_folder_path + f'/cluster{region}.bin', format='I')
-        # records = read_multiple_records_by_id(file_path=data_file_path, records_id=region_ids, dictionary_format=True)
         records=read_binary_file_chunk(index_folder_path+f'/cluster{region}.bin', f'I{70}f', 0, chunk_size=100000000000,dictionary_format=True)
-        # print("region",region,"&& len:",len(records))
 
 
         # Vectorize cosine similarity calculation
@@ -166,13 +125,9 @@ def semantic_query_ivf(data_file_path, index_folder_path, query, top_k, n_region
                 heapq.heappush(top_scores_heap, (score, id))
             else:
                 heapq.heappushpop(top_scores_heap, (score, id))
-
-        # end_loop = time.time()
-        # print("1 Region  Time",end_loop - start_loop)
+        
     # Sort and get the top k scores
     top_scores_heap.sort(reverse=True)
     top_k_ids = [id for _, id in top_scores_heap]
-    # end = time.time()
-    # print("Time Stamp(3) ",end - start)
-
+    
     return top_k_ids
